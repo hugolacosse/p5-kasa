af33e925baad228114ddba13b15057663ee648fb
hugolacosse <hugolacosse.pro@gmail.com>
2025-08-21 15:50:21 +0200

    update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,21 @@
+import './footer.module.css';
+
+/*
+font-family: Montserrat;
+font-weight: 500;
+font-size: 24px;
+leading-trim: NONE;
+line-height: 143%;
+letter-spacing: 0%;
+text-align: center;
+vertical-align: bottom;
+*/
+
 function Footer() {
   return (
     <footer>
       <h1>Kasa</h1>
-      <p>Copyright All right deserved</p>
+      <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
 }
