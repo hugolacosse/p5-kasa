c21c13f087f9e6ff5b8b36e44ec965b7af8c565d
hugolacosse <hugolacosse.pro@gmail.com>
2025-08-28 11:39:59 +0200

    fix fetch path and Footer display

--- src/api/fetchItems.js
+++ src/api/fetchItems.js
@@ -1,6 +1,6 @@
 const fetchItems = async () => {
   try {
-    const response = await fetch('public/logements.json');
+    const response = await fetch('/logements.json');
     if (!response.ok) throw Error('error: !response.ok');
 
     const listItems = await response.json();
