3207ebbf21166be1811cb1ea7af5fd82558c5845
hugolacosse <hugolacosse.pro@gmail.com>
2025-09-02 11:41:54 +0200

    fix Carousel display

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react';
 function Carousel({ pictures }) {
   const [index, setIndex] = useState(0);
 
+  /* Changes carousel image after click */
   function handleClick(e) {
     if (e == 'left') {
       if (index == 0) {
@@ -23,6 +24,8 @@ function Carousel({ pictures }) {
     }
   }
 
+  /* If there is only one image, don't show the buttons and the numbering */
+
   return (
     <section className={styles.carousel}>
       <img
@@ -30,21 +33,25 @@ function Carousel({ pictures }) {
         src={pictures[index]}
         alt="A photo of the logement."
       />
-      <img
-        onClick={(e) => handleClick('left')}
-        className={styles.carousel__leftArrow}
-        src={leftArrow}
-        alt="Control arrow left"
-      />
-      <img
-        onClick={(e) => handleClick('right')}
-        className={styles.carousel__rightArrow}
-        src={rightArrow}
-        alt="Control arrow right"
-      />
-      <span
-        className={styles.carousel__numbering}
-      >{`${index + 1}/${pictures.length}`}</span>
+      {pictures?.length > 1 ? (
+        <>
+          <img
+            onClick={(e) => handleClick('left')}
+            className={styles.carousel__leftArrow}
+            src={leftArrow}
+            alt="Control arrow left"
+          />
+          <img
+            onClick={(e) => handleClick('right')}
+            className={styles.carousel__rightArrow}
+            src={rightArrow}
+            alt="Control arrow right"
+          />
+          <span
+            className={styles.carousel__numbering}
+          >{`${index + 1}/${pictures.length}`}</span>
+        </>
+      ) : null}
     </section>
   );
 }
